8d436958331a44233782c97baab22159a827520c
TheCodeWizard27 <benny.schaerer@gmail.com>
2025-10-21 16:04:35 +0200

    Added lock on feature.

--- src/core/camera_controller/camera_controller.gd
+++ src/core/camera_controller/camera_controller.gd
@@ -2,32 +2,40 @@ class_name CameraController
 extends Node3D
 
 @export_range(0, 1) var mouse_sensitivity: float = 0.2
-@export var active: bool = true:
-	get:
-		return active
-	set(value):
-		active = value
-		active_changed.emit(value)
+@export var camera: Camera3D
 
-signal active_changed
+var target: Node3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+func _process(delta: float) -> void:
+	if (target):
+		_move_towards_target(delta)
+
+func _move_towards_target(delta: float) -> void:
+	var current_rotation = rotation
+	var direction_to_target = global_position.direction_to(target.global_position)
+	var target_rotation = Basis.looking_at(direction_to_target)
+
+	basis = basis.slerp(target_rotation, delta * 2)
+	rotation.x = current_rotation.x
+	rotation.z = current_rotation.z
+
 func _input(event: InputEvent) -> void:
-	if(!active):
+	if (!camera or !camera.current):
 		return
 	
-	if(event.is_action("click")):
+	if (event.is_action("click")):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-	if(event.is_action_pressed("toggle_mouse_captured")):
-		if(Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
+	if (event.is_action_pressed("toggle_mouse_captured")):
+		if (Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-	if(event is InputEventMouseMotion):
+	if (event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
 		rotation_degrees.x = lerpf(rotation_degrees.x, clamp(rotation_degrees.x, -90, 30), 1)
 		
--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -1,13 +1,6 @@
 class_name Player
 extends Node3D
 
-@export var active: bool = true:
-	get:
-		return active
-	set(value):
-		active = value
-		Utils.call_when_ready(self, func(): _set_camera_active(value))
-
 @export var max_speed: float = Globals.PLAYER_MAX_SPEED
 @export var acceleration: float = Globals.PLAYER_ACCELERATION
 @export var min_jump_height: float = Globals.PLAYER_MIN_JUMP_HEIGHT
@@ -15,15 +8,18 @@ extends Node3D
 @export var jump_time: float = Globals.PLAYER_JUMP_TIME
 @export var turn_threshold: float = Globals.ENTITY_TURN_THRESHOLD
 
+@onready var _hud: Hud = %Hud
 @onready var _state: StateMachine = %StateMachine
 @onready var _body: CharacterBody3D = %Body
 @onready var _animator: PlayerAnimator = %PlayerAnimator
 @onready var _camera_mount: CameraController = %CameraMount
 
+@onready var camera: Camera3D = %Camera
+
 var status: PlayerStatus
 var currency: int
 var is_dead: bool = false
-
+var active: bool = true
 var target: Node3D
 
 func _test_damage(damage: int) -> void:
@@ -45,8 +41,8 @@ func process_movement(delta: float, speed_modifier: float = 1) -> void:
 	
 	# Rotate Character body	
 	if(move_dir.length() > turn_threshold):
-		var target = Vector3.BACK.signed_angle_to(move_dir, Vector3.UP)
-		_body.rotation.y = lerp_angle(_body.rotation.y, target, 10 * delta)
+		var target_angle = Vector3.BACK.signed_angle_to(move_dir, Vector3.UP)
+		_body.rotation.y = lerp_angle(_body.rotation.y, target_angle, delta)
 	
 func _ready() -> void:
 	configure_player(PlayerStatus.new())
@@ -66,11 +62,8 @@ func _process(delta: float) -> void:
 		is_dead = true
 		_animator.play_death()
 	
-	if (target):
-		var rotation = _camera_mount.rotation
-		_camera_mount.look_at_from_position(_camera_mount.position, target.position, Vector3.UP)
-		_camera_mount.rotation.x = rotation.x
-		_camera_mount.rotation.z = rotation.z
+	_hud.update_target_indicator(target, delta)
+	_camera_mount.target = target
 	
 	_state.process(delta)
 
@@ -90,8 +83,8 @@ func _apply_movement(delta: float) -> void:
 	var movement = Vector2(_body.velocity.x, _body.velocity.z)
 	
 	if(movement.length() > turn_threshold):
-		var target = Quaternion(Vector3.UP, Vector2(_body.velocity.z, _body.velocity.x).angle())
-		_body.basis = _body.basis.slerp(target, 0.2)
+		var target_angle = Quaternion(Vector3.UP, Vector2(_body.velocity.z, _body.velocity.x).angle())
+		_body.basis = _body.basis.slerp(target_angle, 0.2)
 		
 func get_hit(damage: float, source: Node3D) -> void:
 	status.current_health = max(status.current_health - damage, 0)
