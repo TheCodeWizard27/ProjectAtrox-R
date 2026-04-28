ab9544342d87199e7ddba514a44ccd6459839cc9
TheCodeWizard27 <benny.schaerer@gmail.com>
2026-04-28 16:08:26 +0200

    Added base for Hub area.

--- src/scenes/areas/environment/area_environment.gd
+++ src/scenes/areas/environment/area_environment.gd
@@ -4,6 +4,7 @@ class_name AreaEnvironment
 
 @onready var sun: DirectionalLight3D = $Sun
 @onready var moon: DirectionalLight3D = $Moon
+@onready var world_environment: WorldEnvironment = $WorldEnvironment
 
 # Normalized time from 0.0 to 0.1
 var _time: float = 0.5
@@ -19,6 +20,6 @@ var _time: float = 0.5
 func _update_sun_and_moon() -> void:
 	sun.rotation.x = deg_to_rad(_time * 360)
 	moon.rotation.x = deg_to_rad(_time * 360 - 180.0)
-		
+
 func _ready() -> void:
 	_update_sun_and_moon()
