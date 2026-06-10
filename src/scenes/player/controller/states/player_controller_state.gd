d1395d46878f16b33a0bcb1f40f16f03476a9f56
TheCodeWizard27 <benny.schaerer@gmail.com>
2026-06-10 10:42:42 +0200

    Fixed lock_on_target not being checked against tree before synchronising with player.

--- src/scenes/player/controller/states/player_controller_state.gd
+++ src/scenes/player/controller/states/player_controller_state.gd
@@ -16,6 +16,6 @@ func sync_camera_and_player() -> void:
 	var player = controller.target_player
 	var camera_controller = controller.camera_controller
 	
-	player.lock_on_target = camera_controller.lock_on_target
+	player.lock_on_target = camera_controller.lock_on_target if is_instance_valid(camera_controller.lock_on_target) else null
 	player.looking_direction = -camera_controller.basis.z.normalized()
 	camera_controller.global_position = player.camera_anchor.global_position
